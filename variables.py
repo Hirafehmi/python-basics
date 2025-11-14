3c1619f227e252e23ca6e4f38c081ddc48d6a600
Hirafehmi <155538621+Hirafehmi@users.noreply.github.com>
2025-11-14 22:10:04 +0500

    Add files via upload

---
+++ variables.py
@@ -0,0 +1,8 @@
+# Variables & Data Types
+
+name = "Hira"        # string
+age = 20             # integer
+height = 5.3         # float
+is_student = True    # boolean
+
+print(name, age, height, is_student)
