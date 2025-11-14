e4e662a14a00f297180223a5172fc3045d832f4c
Hirafehmi <155538621+Hirafehmi@users.noreply.github.com>
2025-11-14 22:17:01 +0500

    Add files via upload

---
+++ inout- output.py
@@ -0,0 +1,6 @@
+# Input & Output
+# Uncomment the lines below to test input
+
+# user_name = input("Enter your name: ")
+# user_age = input("Enter your age: ")
+# print("Hello", user_name, "you are", user_age, "years old.")
