3ab7ba84633100319aa19b85d81d962e03d40265
Hirafehmi <155538621+Hirafehmi@users.noreply.github.com>
2025-11-14 22:22:13 +0500

    Add files via upload

---
+++ loops.py
@@ -0,0 +1,11 @@
+# Loops in Python
+
+# For loop
+for i in range(5):
+    print("For Loop Count:", i)
+
+# While loop
+count = 0
+while count < 3:
+    print("While Loop Count:", count)
+    count += 1
